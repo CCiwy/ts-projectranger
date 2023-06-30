3e12abbe3a9c6a1993f3b9204a0050dc104dbbd4
Ciwy <c.walter@afk-dev.de>
2023-06-30 13:21:30 +0200

    add: pages selection interfaces

--- src/pages/selection/index.tsx
+++ src/pages/selection/index.tsx
@@ -2,15 +2,18 @@ import { React, useReducer } from 'react';
 import { ProjectChooser } from './components/';
 import { ExpSelection } from './components/';
 import { LanguageSelection } from './components/';
+import { Selections } from './interfaces';
 
 
-const LANGUAGES = ["PHP","Python","JS", "TS", "C", "C++", "C#", "GO", "Rust", "Lisp", "OCamel", "Cobol", "Lua", "Julia"].sort();
+// todo: should come from backend
+const LANGUAGES: string[] = ["PHP","Python","JS", "TS", "C", "C++", "C#", "GO", "Rust", "Lisp", "OCamel", "Cobol", "Lua", "Julia"].sort();
 
 
 //todo: refactor as context provider
 // https://stackoverflow.com/questions/55055793/react-usereducer-hook-fires-twice-how-to-pass-props-to-reducer
 
-const INITAL_SELECTIONS = {
+
+const INITAL_SELECTIONS: Selections = {
     skill: '',
     languages: {
         prefered: [],
@@ -18,17 +21,27 @@ const INITAL_SELECTIONS = {
     }
 }
 
-export function SelectionWrapper() {
+// todo: import from seperate file
+// also use in child
+enum ActionType {
+    lang =  'language',
+    skill =  'skill',
+    unknown = 'Uknown Action'
+}
+
+
+const SelectionWrapper: React.FC = () => {
     
     const reducer = (state, action) =>  {
-        console.log(action)
+        //if (action.type === ActionType.language){
         if (action.type === 'language'){
             return {languages: action.payload}
         
+        //} else if  (action.type === ActionType.skill ) {
         } else if  (action.type === 'skill' ) {
             return  {skill: action.payload}
         }
-        throw Error('Unknown action');
+        throw Error(ActionType.Unknown);
     }
 
     const [selections, dispatch] = useReducer(reducer, INITAL_SELECTIONS)
@@ -41,3 +54,6 @@ export function SelectionWrapper() {
         <ProjectChooser state={selections}/>
     </>)
 }
+
+
+export {SelectionWrapper}
