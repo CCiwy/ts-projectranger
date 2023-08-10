d98d9d77d9fe9900de5158b6b26fc0ba311310ef
Ciwy <c.walter@afk-dev.de>
2023-08-10 15:28:51 +0200

    intermediate commit

--- src/pages/selection/components/index.ts
+++
@@ -1,6 +0,0 @@
-import ProjectChooser from './Projectchooser';
-import ExpSelection from './Experience';
-import LanguageSelection from './Language';
-
-export { ProjectChooser, ExpSelection, LanguageSelection }
-
--- src/pages/selection/interfaces.ts
+++
@@ -1,9 +0,0 @@
-export interface LangSelections {
-    prefered: string[],
-    dismissed: string[],
-}
-
-export interface Selections  {
-    skill: string, // todo: should be enum
-    languages: LangSelections 
-}
